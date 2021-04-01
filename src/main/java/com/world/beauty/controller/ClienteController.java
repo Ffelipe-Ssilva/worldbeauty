92017c2a21324f8a78a9c66238fff4483ea1309b
Flint-Silver <ffelipe.ssilva02@gmail.com>
2021-03-31 21:02:40 -0300

    segundo commit

--- src/main/java/com/world/beauty/controller/ClienteController.java
+++ src/main/java/com/world/beauty/controller/ClienteController.java
@@ -2,19 +2,38 @@ package com.world.beauty.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfigureOrder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.world.beauty.model.Cliente;
+import com.world.beauty.repository.ClienteRepository;
 
 @RestController
 @RequestMapping("/clientes")
 public class ClienteController {
 	
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
 	@GetMapping
 	public List<Cliente> listar() {
+		return clienteRepository.findAll();
+	}
+	
+	@GetMapping("/falar")
+	public String falar() {
 		return "oi";
+	}
+	
+	@PostMapping
+	public Cliente adicionar(@RequestBody Cliente cliente) {
+	return clienteRepository.save(cliente);
 	}
 
 }
--- src/main/java/com/world/beauty/model/Cliente.java
+++ src/main/java/com/world/beauty/model/Cliente.java
@@ -1,12 +1,31 @@
 package com.world.beauty.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
 @Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Cliente {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "cliente_id")
 	private Long id;
+	@Column(nullable=false)
 	private String nome;
 	private String telefone;
+	@Column(nullable=false)
 	private String dtnasc;
+	@Column(nullable=false)
 	private String genero;
 }
