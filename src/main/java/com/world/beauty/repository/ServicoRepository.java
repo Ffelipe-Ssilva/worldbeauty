4741f52d88d0d46d83c2a85177829aaf72c80408
Flint-Silver <ffelipe.ssilva02@gmail.com>
2021-04-19 20:19:13 -0300

    popularidade por genero

--- src/main/java/com/world/beauty/repository/ServicoRepository.java
+++ src/main/java/com/world/beauty/repository/ServicoRepository.java
@@ -20,7 +20,6 @@ public interface ServicoRepository extends JpaRepository<Servico, Long>{
 	@Query(value = "SELECT count(servico.nome) as frequencia,servico.nome,servico.servico_id FROM servico INNER JOIN pedidos ON pedidos.servico_id = servico.servico_id GROUP BY servico.nome,servico.servico_id ORDER BY frequencia desc", nativeQuery = true)
 	List<Servico> maiorProcura();
 	
-	@Query(value = "SELECT count(servico.nome) as frequencia,servico.nome,servico.servico_id FROM pedidos INNER JOIN cliente ON pedidos.cliente_id = cliente.cliente_id INNER JOIN servico ON pedidos.servico_id = "
-			+ "servico.servico_id WHERE genero=:genero GROUP BY servico.nome,servico.servico_id ORDER BY frequencia desc", nativeQuery = true)
+	@Query(value = "SELECT count(servico.nome) as frequencia,servico.nome,servico.servico_id FROM pedidos INNER JOIN servico ON pedidos.servico_id = servico.servico_id INNER JOIN cliente ON cliente.cliente_id=pedidos.cliente_id WHERE genero=:genero GROUP BY servico.nome,servico.servico_id ORDER BY frequencia desc", nativeQuery = true)
 	List<Servico> maiorProcuraGenero(@Param("genero") String genero);
 }
